48f5f73d91f9a5104bad714de15ddf68238b82a7
yl790 <yitao@rstudio.com>
2020-06-24 17:26:00 +0000

    scale = 1 to compare abs diff
    Signed-off-by: yl790 <yitao@rstudio.com>

--- tests/testthat/test-summarizers.R
+++ tests/testthat/test-summarizers.R
@@ -37,17 +37,20 @@ test_that("weighted-avg summarizer works as expected", {
   expect_equal(
     ts_weighted_avg$v_w_weightedMean,
     c(4, 2, 2, 2.66666667, 5, 1, -4, -1, -1, 3.4),
-    tolerance = 1e-8
+    tolerance = 1e-7,
+    scale = 1
   )
   expect_equal(
     ts_weighted_avg$v_w_weightedStandardDeviation,
     c(NaN, 4.2426407, 4.2426407, 4.9497475, NaN, NaN, NaN, 6.3639610, 6.3639610, 1.4142136),
-    tolerance = 1e-8
+    tolerance = 1e-7,
+    scale = 1
   )
   expect_equal(
     ts_weighted_avg$v_w_weightedTStat,
     c(NaN, 0.63245553, 0.63245553, 0.72280632, NaN, NaN, NaN, -0.21081851, -0.21081851, 2.91547595),
-    tolerance = 1e-8
+    tolerance = 1e-7,
+    scale = 1
   )
   expect_equal(ts_weighted_avg$v_w_observationCount, c(1, 2, 2, 2, 1, 1, 1, 2, 2, 2))
 })
