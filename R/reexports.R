a85afbda7623940187d8eded6f4914802b7d99aa
Yitao Li <yitao@rstudio.com>
2020-09-15 12:37:42 +0000

    fix reexport.R
    Signed-off-by: Yitao Li <yitao@rstudio.com>

---
+++ R/reexports.R
@@ -0,0 +1,11 @@
+#' @include sdf_utils.R
+NULL
+
+#' Retrieve a Spark DataFrame
+#'
+#' See \code{\link[sparklyr:spark_dataframe]{spark_dataframe}} for more details.
+#'
+#' @name spark_dataframe
+#' @export
+#' @importFrom sparklyr spark_dataframe
+NULL
