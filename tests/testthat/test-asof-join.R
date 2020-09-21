2cc1ce4f1f40a2b820fec8293adda5d77be9c726
Yitao Li <yitao@rstudio.com>
2020-09-21 15:21:51 +0000

    test cases for asof join with key columns
    Signed-off-by: Yitao Li <yitao@rstudio.com>

--- tests/testthat/test-asof-join.R
+++ tests/testthat/test-asof-join.R
@@ -1,22 +1,47 @@
 context("asof-join")
 
 sc <- testthat_spark_connection()
-ts_1 <- copy_to(sc, tibble::tibble(t = seq(10), u = seq(10))) %>%
+
+ts_1 <- copy_to(
+  sc,
+  tibble::tibble(t = seq(10), u = seq(10), k1 = rep(c(0L, 1L), 5), k2 = rep(c(1L, 0L), 5))
+) %>%
   from_sdf(is_sorted = TRUE, time_unit = "SECONDS", time_column = "t")
-ts_2 <- copy_to(sc, tibble::tibble(t = seq(10) + 1, v = seq(10) + 1L)) %>%
+
+ts_2 <- copy_to(
+  sc,
+  tibble::tibble(t = seq(10) + 1, v = seq(10) + 1L)
+) %>%
   from_sdf(is_sorted = TRUE, time_unit = "SECONDS", time_column = "t")
-ts_3 <- copy_to(sc, tibble::tibble(t = seq(10) * 2, w = seq(10))) %>%
+
+ts_3 <- copy_to(
+  sc,
+  tibble::tibble(t = seq(10) * 2, w = seq(10))
+) %>%
+  from_sdf(is_sorted = TRUE, time_unit = "SECONDS", time_column = "t")
+
+ts_2_with_key_cols <- copy_to(
+  sc,
+  tibble::tibble(t = seq(10) + 1, v = seq(10) + 1L, k1 = 0L, k2 = 1L)
+) %>%
+  from_sdf(is_sorted = TRUE, time_unit = "SECONDS", time_column = "t")
+
+ts_3_with_key_cols <- copy_to(
+  sc,
+  tibble::tibble(t = seq(10) * 2, w = seq(10), k1 = 1L, k2 = 0L)
+) %>%
   from_sdf(is_sorted = TRUE, time_unit = "SECONDS", time_column = "t")
 
 test_that("asof_join works with direction = \">=\"", {
   rs <- asof_join(ts_2, ts_1, direction = ">=") %>% collect()
-
   expect_equivalent(
     rs,
     tibble::tibble(
       t = as.POSIXct(seq(10) + 1, origin = "1970-01-01"),
       v = seq(10) + 1,
-      u = c(seq(9) + 1, NA)
+      u = c(seq(9) + 1, NA),
+      k1 = c(rep(c(1L, 0L), 4), 1L, NA),
+      k2 = c(rep(c(0L, 1L), 4), 0L, NA)
     )
   )
 
@@ -26,7 +51,9 @@ test_that("asof_join works with direction = \">=\"", {
     tibble::tibble(
       t = as.POSIXct(seq(10), origin = "1970-01-01"),
       u = seq(10),
-      v = c(NA, seq(9) + 1),
+      k1 = rep(c(0L, 1L), 5),
+      k2 = rep(c(1L, 0L), 5),
+      v = c(NA, seq(9) + 1)
     )
   )
 
@@ -36,7 +63,9 @@ test_that("asof_join works with direction = \">=\"", {
     tibble::tibble(
       t = as.POSIXct(seq(10) * 2, origin = "1970-01-01"),
       w = seq(10),
-      u = c(seq(5) * 2, rep(NA, 5))
+      u = c(seq(5) * 2, rep(NA, 5)),
+      k1 = c(rep(1L, 5), rep(NA, 5)),
+      k2 = c(rep(0L, 5), rep(NA, 5))
     )
   )
 
@@ -46,19 +75,22 @@ test_that("asof_join works with direction = \">=\"", {
     tibble::tibble(
       t = as.POSIXct(seq(10) * 2, origin = "1970-01-01"),
       w = seq(10),
-      u = c(seq(5) * 2, 10L, 10L, rep(NA, 3))
+      u = c(seq(5) * 2, 10L, 10L, rep(NA, 3)),
+      k1 = c(rep(1L, 7), rep(NA, 3)),
+      k2 = c(rep(0L, 7), rep(NA, 3))
     )
   )
 })
 
 test_that("asof_join works with direction = \"<=\"", {
   rs <- asof_join(ts_1, ts_2, direction = "<=") %>% collect()
-
   expect_equivalent(
     rs,
     tibble::tibble(
       t = as.POSIXct(seq(10), origin = "1970-01-01"),
       u = seq(10),
+      k1 = rep(c(0L, 1L), 5),
+      k2 = rep(c(1L, 0L), 5),
       v = c(NA, seq(9) + 1)
     )
   )
@@ -70,6 +102,8 @@ test_that("asof_join works with direction = \"<=\"", {
       t = as.POSIXct(seq(10) + 1, origin = "1970-01-01"),
       v = seq(10) + 1,
       u = c(seq(9) + 1, NA),
+      k1 = c(rep(c(1L, 0L), 4), 1L, NA),
+      k2 = c(rep(c(0L, 1L), 4), 0L, NA)
     )
   )
 
@@ -79,7 +113,9 @@ test_that("asof_join works with direction = \"<=\"", {
     tibble::tibble(
       t = as.POSIXct(seq(10) * 2, origin = "1970-01-01"),
       w = seq(10),
-      u = c(seq(5) * 2, rep(NA, 5))
+      u = c(seq(5) * 2, rep(NA, 5)),
+      k1 = c(rep(1L, 5), rep(NA, 5)),
+      k2 = c(rep(0L, 5), rep(NA, 5))
     )
   )
 
@@ -89,6 +125,8 @@ test_that("asof_join works with direction = \"<=\"", {
     tibble::tibble(
       t = as.POSIXct(seq(10), origin = "1970-01-01"),
       u = seq(10),
+      k1 = rep(c(0L, 1L), 5),
+      k2 = rep(c(1L, 0L), 5),
       v = c(2, seq(9) + 1)
     )
   )
@@ -101,6 +139,8 @@ test_that("asof_join works with direction = \"<\"", {
     tibble::tibble(
       t = as.POSIXct(seq(10), origin = "1970-01-01"),
       u = seq(10),
+      k1 = rep(c(0L, 1L), 5),
+      k2 = rep(c(1L, 0L), 5),
       v = NA_integer_
     )
   )
@@ -111,6 +151,8 @@ test_that("asof_join works with direction = \"<\"", {
     tibble::tibble(
       t = as.POSIXct(seq(10), origin = "1970-01-01"),
       u = seq(10),
+      k1 = rep(c(0L, 1L), 5),
+      k2 = rep(c(1L, 0L), 5),
       v = NA_integer_
     )
   )
@@ -121,6 +163,8 @@ test_that("asof_join works with direction = \"<\"", {
     tibble::tibble(
       t = as.POSIXct(seq(10), origin = "1970-01-01"),
       u = seq(10),
+      k1 = rep(c(0L, 1L), 5),
+      k2 = rep(c(1L, 0L), 5),
       v = c(seq(10) + 1)
     )
   )
@@ -183,6 +227,8 @@ test_that("asof_join works with left_prefix and right_prefix", {
     tibble::tibble(
       t = as.POSIXct(seq(10), origin = "1970-01-01"),
       left_u = seq(10),
+      left_k1 = rep(c(0L, 1L), 5),
+      left_k2 = rep(c(1L, 0L), 5),
       right_v = c(NA, seq(9) + 1)
     )
   )
@@ -195,7 +241,55 @@ test_that("asof_join works with left_prefix and right_prefix", {
     tibble::tibble(
       t = as.POSIXct(seq(10), origin = "1970-01-01"),
       left_u = seq(10),
+      left_k1 = rep(c(0L, 1L), 5),
+      left_k2 = rep(c(1L, 0L), 5),
       right_v = c(2, seq(9) + 1)
+    )
+  )
+})
+
+test_that("asof_join works with key columns", {
+  rs <- asof_join(
+    ts_2_with_key_cols, ts_1, direction = ">=", key_columns = c("k1", "k2")
+  ) %>%
+    collect()
+  expect_equivalent(
+    rs,
+    tibble::tibble(
+      t = as.POSIXct(seq(10) + 1, origin = "1970-01-01"),
+      v = seq(10) + 1,
+      k1 = 0L,
+      k2 = 1L,
+      u = c(NA_integer_, 3, NA, 5, NA, 7, NA, 9, NA, NA)
+    )
+  )
+
+  rs <- asof_join(
+    ts_1, ts_2_with_key_cols, direction = "<=", key_columns = c("k1", "k2")
+  ) %>%
+    collect()
+  expect_equivalent(
+    rs,
+    tibble::tibble(
+      t = as.POSIXct(seq(10), origin = "1970-01-01"),
+      u = seq(10),
+      k1 = rep(c(0L, 1L), 5),
+      k2 = rep(c(1L, 0L), 5),
+      v = c(NA_integer_, NA, 3, NA, 5, NA, 7, NA, 9, NA)
+    )
+  )
+
+  rs <- asof_join(
+    ts_1, ts_2_with_key_cols, direction = "<", tol = "1s", key_columns = c("k1", "k2")
+  ) %>% collect()
+  expect_equivalent(
+    rs,
+    tibble::tibble(
+      t = as.POSIXct(seq(10), origin = "1970-01-01"),
+      u = seq(10),
+      k1 = rep(c(0L, 1L), 5),
+      k2 = rep(c(1L, 0L), 5),
+      v = c(2L, NA, 4L, NA, 6L, NA, 8L, NA, 10L, NA)
     )
   )
 })
